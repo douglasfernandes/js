ab10c781789e85dc2f3495bfdceb2990292aa591
Douglas Fernandes <douglasfernandes@gmail.com>
2018-10-09 22:34:01 -0300

    curso udemy-react incluido.

---
+++ react/aula1/app.js
@@ -0,0 +1,9 @@
+class Channel extends RTCIceCandidate.Component{
+    render(){
+        return (
+            <li>Channel name</li>
+        )
+    }
+}
+
+ReactDOM.render(<Chanel/>, document.getElementById('app'));
